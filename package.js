280e2493c79e56e95c1cb3a30152d42e54f156de
imkost <imkost@yandex.ru>
2015-09-05 00:23:23 +0300

    Added version for ecmascript

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
 
-  api.use('ecmascript');
+  api.use('ecmascript@0.1.3-rc.2');
 
   api.addFiles([
     'fragments/fragments.js',
